6a6ccc589e311e5af0ba330adbb4bc5fa5d969ab
Filipe Silva <fil.lourenco.silva@gmail.com>
2024-03-22 14:33:39 -0300

    benchmarks: ienumerables traiçoeiros

---
+++ IEnumerablesTraicoeiros/Program.cs
@@ -0,0 +1,52 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkSwitcher.FromAssembly(typeof(BenchmarkColecoesMaterializadas).Assembly).Run(args);
+
+return;
+
+[SimpleJob]
+[MemoryDiagnoser]
+public class BenchmarkColecoesMaterializadas
+{
+    [Params(100, 1_000)]
+    public int tamanho;
+
+    private List<int> ObterList()
+    {
+        var lista = new List<int>();
+
+        for (var i = 0; i < tamanho; i++)
+            lista.Add(i);
+
+        return lista;
+    }
+
+    private IEnumerable<int> ObterYield()
+    {
+        for (var i = 0; i < tamanho; i++)
+            yield return i;
+    }
+
+    [Benchmark]
+    public int BenchmarkList()
+    {
+        var sum = 0;
+
+        foreach (var item in ObterList())
+            sum += item;
+
+        return sum;
+    }
+
+    [Benchmark]
+    public int BenchmarkIEnumerable()
+    {
+        var sum = 0;
+
+        foreach (var item in ObterYield())
+            sum += item;
+
+        return sum;
+    }
+}
